e8ae6d0c8fb9a58c2105e6181a31c613952a0c1d
JessicaBarnett <BarnettEffect@gmail.com>
2014-06-11 23:51:35 -0400

    Completed Odd/Even headers assignment

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+/********  PYRAMID **********/
 
 function pyramid(char, lines){
 	document.write("<pre>");
@@ -6,16 +7,32 @@ function pyramid(char, lines){
 
 		for (i = 0; i<h; i++){
 			document.write(char);
-			console.log(char);
 		}
-		document.write("\n");//so, html ignores whitespace characters and uses them as spaces...
+		document.write("\n");
 	}
 	document.write("</pre>");
 
 }
 
+/********  HEADERS **********/
+
+function oddEvenHeaders(highestHeader){
+	var html= "";
+	for (h = 1; h < highestHeader; h++){
+		html += "<h" + h + ">" + evenOrOdd(h) +"</h" + h+ ">\n";
+	}
+	return html;
+}
+
+function evenOrOdd(val){
+	if (val %2 === 1)
+		return "ODD";
+	else return "EVEN";
+}
 
 /**********  START  **************/
+
 pyramid("*", 8);
+document.write(oddEvenHeaders(6));
 
 
